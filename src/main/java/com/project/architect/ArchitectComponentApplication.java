9bb5017c41ebf02d3e4df9b49f7ca229da7eac6c
chathurkavishmantha <chathurkavish@gmail.com>
2021-03-14 22:38:12 +0530

    import

---
+++ src/main/java/com/project/architect/ArchitectComponentApplication.java
@@ -0,0 +1,13 @@
+package com.project.architect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectComponentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/architect/exception/ResourceNotFoundExceptions.java
@@ -0,0 +1,14 @@
+package com.project.architect.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundExceptions extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundExceptions(String message) {
+		super(message);
+	}
+}
